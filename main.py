5a43e20467890a7be618581772fa971282147556
Sabrina-0324 <z.sabrina2003@gmail.com>
2026-04-10 17:39:51 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "API bibliothèque fonctionne !"}
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Remplace USER, PASSWORD et DBNAME par tes informations
+DATABASE_URL = "postgresql+psycopg2://USER:PASSWORD@localhost:5432/library_db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
